0f72586b7a273f3cb9337a9f1931ccc1399c9afe
Jacob McCarthy <thejacobmccarthy@gmail.com>
2022-04-19 22:41:16 -0400

    got copy/paste working, doing some styling tweaks

--- index.js
+++ index.js
@@ -27,10 +27,19 @@ const displayColors = (colors) => {
   const colorStopArr = document.querySelectorAll(".color__stop");
 
   for (let i=0; i < colors.length; i++) {
-    colorStopArr[i].style.background = `rgba(${colors[i]})`
+    colorStopArr[i].style.background = `rgba(${colors[i]})`;
+    colorStopArr[i].dataset.rgb = `rgba(${colors[i]})`;
+    // colorStopArr[i].textContent = `rgba(${colors[i]})`;
   }
 };
+
+const copyColorCode = (e) => {
+  const colorCode = e.target.dataset.rgb;
+  navigator.clipboard.writeText(colorCode);
+  console.log(colorCode);
+}
 
 
 generateColors();
 document.querySelector(".generate__btn").addEventListener("click", generateColors);
+document.querySelector(".palette__container").addEventListener('click', copyColorCode);
